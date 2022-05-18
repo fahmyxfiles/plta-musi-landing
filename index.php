c5b7a46708780fa4c5f3ed21399050d14c0ead8e
fahmyxfiles <fahmy.xfiles@gmail.com>
2022-05-19 00:31:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+
+  <!-- my css -->
+  <link rel="stylesheet" href="mycss.css" />
+
+  <!-- google fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet" />
+
+  <title>WELCOME PLTA MUSI</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img width="40px" src="img/pln.png"> PLTA MUSI</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    </div>
+    </div>
+  </nav>
+  <!-- akhir navbar -->
+
+  <!-- jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">
+        Pembangkit Listrik <br> Tenaga
+        Air MUSI<br> <br><span>LOGIN</span>
+      </h1>
+      <a href="login.php" class="btn btn-primary tombol">USER</a>
+      <a href="login.php" class="btn btn-primary tombol">SUPERUSER</a>
+    </div>
+  </div>
+  <!-- akhir jumbotron -->
+
+
+  <section class="testimonial">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <h4><b>ALAMAT</b></h4>
+        <h5>
+          Ujan MAS Atas, Ujan MAS, Kabupaten Kepahiang, Bengkulu 38386
+        </h5>
+        <p>PLTA MUSI (Bendungan)</p>
+      </div>
+    </div>
+
+
+    <!-- footer -->
+    <div class="row footer">
+      <div class="col text-center">
+        <p>2020 PLTA MUSI Copyright.</p>
+      </div>
+    </div>
+    <!-- akhir footer -->
+    <!-- akhir container -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ on-member/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+
+    <!-- my css -->
+    <link rel="stylesheet" href="mycss.css" />
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet" />
+
+    <title>PLTA MUSI</title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img width="40px" src="img/pln.png"> PLTA MUSI</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active" href="#"><b> WO <span class="sr-only">(current)</span></b></a>
+                    <a class="nav-item nav-link" href="#"><b> TOOLS </b></a>
+                    <a class="nav-item btn btn-primary tombol" href="./../logout.php">LOGOUT</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">
+                SELAMAT DATANG <br />
+                <span>USER</span>
+            </h1>
+            <a href="#about" class="btn btn-primary tombol">About US</a>
+        </div>
+    </div>
+    <!-- akhir jumbotron -->
+
+    <!-- container -->
+    <div class="container">
+        <!-- info panel -->
+        <div class="row justify-content-center">
+            <div class="col-10 info-panel">
+                <div class="row">
+                    <div class="col-lg">
+                        <img src="img/employee.png" alt="employ" class="float-left" />
+                        <h4>24 Hours</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="img/hires.png" alt="hires" class="float-left" />
+                        <h4>High-Res</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="img/security.png" alt="secure" class="float-left" />
+                        <h4>Security</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- akhir info panel -->
+
+        <!-- workingspace -->
+        <section id="about">
+            <div class="row workingspace">
+                <div class="col-lg-6">
+                    <img src="img/suasana.png" alt="workingspace" class="img-fluid" />
+                </div>
+                <div class="col-lg-5">
+                    <h4>
+                        <span>PLTA MUSI</span>
+                    </h4>
+                    <p>
+                        Pejabat PT PLN Wilayah Sumatra Selatan, Jambi dan Bengkulu (WS2JB) menyebutkan listrik yang dihasil oleh PLTA Musi di Kabupaten Kepahiang, Provinsi Bengkulu, saat ini menjadi pemasok kebutuhan listrik di Sumatra. <br> Manajer Unit Pelaksana Pengendalian Pembangkitan atau UPDK Bengkulu, Yuliandra saat menjelaskan bahwa energi listrik yang dihasilkan oleh pembangkit ramah lingkungan tersebut memiliki kapasitas 370 MW atau 210 MW, dengan total potensi energi tahunan yang dihasilkan mencapai 1.140 GWH.
+                    </p>
+
+                </div>
+            </div>
+        </section>
+        <!-- akhir workingspace -->
+    </div>
+
+    <section class="testimonial">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h4><b>ALAMAT</b></h4>
+                <h5>
+                    Ujan MAS Atas, Ujan MAS, Kabupaten Kepahiang, Bengkulu 38386
+                </h5>
+                <p>PLTA MUSI (Bendungan)</p>
+            </div>
+        </div>
+
+
+        <!-- footer -->
+        <div class="row footer">
+            <div class="col text-center">
+                <p>2020 PLTA MUSI Copyright.</p>
+            </div>
+        </div>
+        <!-- akhir footer -->
+        <!-- akhir container -->
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
